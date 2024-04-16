5de5d6a8624c274944b954553eb6c660cb897027
estebanEV <estebanechavarriav@gmail.com>
2024-04-16 10:50:14 -0500

    Fix issue converting content time to be displayed

--- lib.php
+++ lib.php
@@ -729,8 +729,8 @@ function goone_convert_hours_mins($time, $format = '%02d:%02d') {
     if ($time < 1) {
         return;
     }
-    $hours = floor($time / 60);
-    $minutes = ($time % 60);
+    $hours = floor(floatval($time) / 60);
+    $minutes = (floatval($time) % 60);
 
     return sprintf($format, $hours, $minutes);
 }
